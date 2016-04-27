f45743ffb862ca9326bbe388a7da757f2c439a20
pimax <m.pinyugin@gmail.com>
2016-04-27 23:45:11 +0300

    fixed

--- Job4JoyBot.php
+++ Job4JoyBot.php
@@ -12,12 +12,16 @@ class Job4JoyBot
         ]);
     }
 
-    public static function curl_request($client_id, $client_secret, $url, $post_data = null) {
-
+    public static function curl_request($client_id, $client_secret, $url, $post_data = null)
+    {
         $headers = [
             'Content-Type: application/x-www-form-urlencoded',
             'Authorization: Basic '. base64_encode($client_id.":".$client_secret)
         ];
+
+        if (is_array($post_data)) {
+            $post_data = array_map(array('Job4JoyBot', 'sanitize_curl_parameter'), $post_data);
+        }
 
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
@@ -25,16 +29,21 @@ class Job4JoyBot
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_TIMEOUT, 15);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
 
-
-        if (is_array($post_data)) {
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
-        }
         $response = curl_exec($ch);
-
         curl_close($ch);
 
         return $response;
+    }
+
+    public static function sanitize_curl_parameter ($value)
+    {
+        if ((strlen($value) > 0) && ($value[0] === '@')) {
+            return substr_replace($value, '&#64;', 0, 1);
+        }
+
+        return $value;
     }
 }
