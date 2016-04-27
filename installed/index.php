39c3fb670a1283b88844c04e781de9a31df55389
pimax <m.pinyugin@gmail.com>
2016-04-27 23:01:16 +0300

    fixed

--- installed/index.php
+++ installed/index.php
@@ -5,7 +5,7 @@ require_once (dirname(__FILE__) .'/../Job4JoyBot.php');
 $data = json_decode(file_get_contents("php://input"), true);
 
 if (!empty($data)) {
-    file_put_contents(__DIR__. '/../data/'.$data['group_id'].'.php', "<?php\nreturn " . var_export($data, true) . ";\n");
+    file_put_contents(__DIR__. '/../data/'.$data['oauthId'].'.php', "<?php\nreturn " . var_export($data, true) . ";\n");
     $token = Job4JoyBot::requestToken($data['oauthId'], $data['oauthSecret'], $data['capabilitiesUrl']);
-    file_put_contents(__DIR__. '/../data/'.$groupConfig['oauthId'].'_token.php', "<?php\nreturn " . var_export($token, true) . ";\n");
+    file_put_contents(__DIR__. '/../data/'.$data['oauthId'].'_token.php', "<?php\nreturn " . var_export($token, true) . ";\n");
 }
